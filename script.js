4b4615ec1bf7ca6449348a6a1e54cc85c2b14dd6
Daniel <danieljanderson0616@gmail.com>
2018-06-21 20:11:02 -0400

    this doesnt work.  doesnt fade when the answer is given the second time.  I dont know why taking the prompt away causes this issue

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+// to do  you have to make the click function display the form.  
+//the form button will then make it shake
+
 window.onload = function(){
-
+  var clickCount = 0
+  //this should fade the answers.
   fade("#answer", "0", "0s");
-
+  var form = document.getElementById("Question")
+  form.style.display = "block"
   var listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
   var onClick = function(){
-    fade("#answer","0","0s")
-    var question = prompt("What do you want to know")
+    form.style.display="none"
+    if (clickCount===1){
+      console.log("in if")
+      fade("#answer","0","7s")
+      document.querySelector("#magic8Ball").setAttribute("src","magic8BallQuestion.png")
+      form.style.display = "block"
+      clickCount=0
+    }
+    //var question = prompt("What do you want to know")
     shake("#magic8Ball")
     var randomIndex = Math.floor((Math.random() * (listofanswers.length)));
     var answer = listofanswers[randomIndex]
     document.querySelector("#magic8Ball").setAttribute("src","magic8BallAnswer.png")
     document.querySelector("#answer").textContent = answer
-    fade("#answer","1","7s") 
+    fade("#answer","1","7s")
+    clickCount++
   };  
   
   document.querySelector("#questionButton").addEventListener("click", onClick);
